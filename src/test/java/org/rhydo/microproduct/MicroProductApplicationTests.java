af9ca5e8b6e6812332da49536b56cb4d7ce216ae
firth-nodot2 <firthnodot2@gmail.com>
2025-07-05 10:14:53 +0800

    First Commit

---
+++ src/test/java/org/rhydo/microproduct/MicroProductApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rhydo.microproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
